9a46ddecb90705e7c1928e3d1048953333ed24cc
fine2006 <animeshpathak.2006@gmail.com>
2024-10-26 18:28:19 +0530

    Setup initial tutorial

---
+++ src/components/Alert.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+  color?: string;
+  onClose: () => void;
+}
+
+const Alert = ({ children, color = "primary", onClose }: Props) => {
+  return (
+    <div className={"alert alert-" + color + " alert-dismissible fade show"}>
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
+    </div>
+  );
+};
+
+export default Alert;
---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+  color?: string;
+  onClick: () => void;
+}
+
+const Button = ({ children, color = "primary", onClick }: Props) => {
+  return (
+    <div className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </div>
+  );
+};
+
+export default Button;
